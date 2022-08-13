1f6601eecc8bc094cac1967af18ce0fc6d884afd
Mark Lavercombe <mlava@users.noreply.github.com>
2022-08-13 20:14:34 +1000

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,5 +1,42 @@
+const config = {
+    tabTitle: "Focus Mode",
+    settings: [
+        {
+            id: "fm-title",
+            name: "Hide Page Title",
+            description: "Hide the Page Title",
+            action: { type: "switch" },
+        },
+        {
+            id: "fm-topbar",
+            name: "Show Topbar",
+            description: "Don't hide Topbar",
+            action: { type: "switch" },
+        },
+        {
+            id: "fm-leftSidebar",
+            name: "Show Left Sidebar",
+            description: "Don't hide Left Sidebar",
+            action: { type: "switch" },
+        },
+        {
+            id: "fm-rightSidebar",
+            name: "Show Right Sidebar",
+            description: "Don't hide Right Sidebar",
+            action: { type: "switch" },
+        },
+        {
+            id: "fm-refs",
+            name: "Show Linked and Unlinked References",
+            description: "Don't hide References",
+            action: { type: "switch" },
+        },
+    ]
+};
+
 export default {
-    onload: ({ }) => {
+    onload: ({ extensionAPI }) => {
+        extensionAPI.settings.panel.create(config);
         window.roamAlphaAPI.ui.commandPalette.addCommand({
             label: "Focus Mode On",
             callback: () => focusModeOn()
@@ -10,13 +47,50 @@ export default {
         });
 
         async function focusModeOn() {
-            await roamAlphaAPI.ui.rightSidebar.close();
-            document.querySelector("#app > div > div > div.flex-h-box > div.roam-main > div.rm-files-dropzone > div").style.visibility = "hidden";
-            document.querySelector("div.rm-reference-main").style.visibility = "hidden";
-            document.querySelector(".roam-body .roam-app .roam-sidebar-container .roam-sidebar-content").style.visibility = "hidden";
-            document.querySelector(".roam-body .roam-app .roam-sidebar-container").style.visibility = "hidden";
+            var fmTitle, fmTopbar, fmLeftSidebar, fmRightSidebar, fmRefs;
+            if (extensionAPI.settings.get("fm-title") == true) {
+                fmTitle = true;
+            } else {
+                fmTitle = false;
+            }
+            if (extensionAPI.settings.get("fm-topbar") == true) {
+                fmTopbar = true;
+            } else {
+                fmTopbar = false;
+            }
+            if (extensionAPI.settings.get("fm-leftSidebar") == true) {
+                fmLeftSidebar = true;
+            } else {
+                fmLeftSidebar = false;
+            }
+            if (extensionAPI.settings.get("fm-rightSidebar") == true) {
+                fmRightSidebar = true;
+            } else {
+                fmRightSidebar = false;
+            }
+            if (extensionAPI.settings.get("fm-refs") == true) {
+                fmRefs = true;
+            } else {
+                fmRefs = false;
+            }
+            if (fmTitle == true) {
+                document.querySelector(".rm-title-display").style.visibility = "hidden";
+            }
+            if (fmTopbar == false) {
+                document.querySelector("#app > div > div > div.flex-h-box > div.roam-main > div.rm-files-dropzone > div").style.visibility = "hidden";
+            }
+            if (fmLeftSidebar == false) {
+                document.querySelector(".roam-body .roam-app .roam-sidebar-container .roam-sidebar-content").style.visibility = "hidden";
+                document.querySelector(".roam-body .roam-app .roam-sidebar-container").style.visibility = "hidden";
+            }
+            if (fmRightSidebar == false) {
+                await roamAlphaAPI.ui.rightSidebar.close();
+            }
+            if (fmRefs == false) {
+                document.querySelector("div.rm-reference-main").style.visibility = "hidden";
+            }
             var matches = document.querySelectorAll("div.roam-log-page");
-            for (var i =1; i < matches.length; i++) {
+            for (var i = 1; i < matches.length; i++) {
                 matches[i].style.visibility = "hidden";
             }
         }
@@ -26,8 +100,9 @@ export default {
             document.querySelector("div.rm-reference-main").style.visibility = "visible";
             document.querySelector(".roam-body .roam-app .roam-sidebar-container .roam-sidebar-content").style.visibility = "visible";
             document.querySelector(".roam-body .roam-app .roam-sidebar-container").style.visibility = "visible";
+            document.querySelector(".rm-title-display").style.visibility = "visible";
             var matches = document.querySelectorAll("div.roam-log-page");
-            for (var i =1; i < matches.length; i++) {
+            for (var i = 1; i < matches.length; i++) {
                 matches[i].style.visibility = "visible";
             }
         }
